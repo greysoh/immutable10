a658e494988c234c5eea6d9472766a59cd0d19cc
greysoh <dev@hofers.cloud>
2023-06-10 16:43:41 -0400

    fix: Adds some proper fixes to Deno lint.
    I just ignore the rest, lol.

--- launcher/libs/waitkeypress.mjs
+++ launcher/libs/waitkeypress.mjs
@@ -1,19 +1,20 @@
+// deno-lint-ignore-file
+// Needed because I need asynchronous promise executors
 import { readKeypress } from "https://deno.land/x/keypress@0.0.11/mod.ts";
 
 export function waitIncludingKeyPress(t) {
   let hasResolved = false;
 
-  // Kindly ignore the error. I'm lazy.
   return new Promise(async(resolve) => {
     async function deathThread() {
       await new Promise((i) => setTimeout(i, t));
       
       hasResolved = true;
       return resolve(false);
-    };
+    }
 
     async function keyThread() {
-      for await (const keypress of readKeypress()) {
+      for await (const _keypress of readKeypress()) {
         if (hasResolved) return;
         return resolve(true);
       }
